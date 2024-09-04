c14ad8cd702673d822a9e4e1ed5db3ab94e7e014
hmakumbu <hmboko@aimsammi.org>
2024-09-04 21:21:40 +0000

    changed env

---
+++ mlops/load_data.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
