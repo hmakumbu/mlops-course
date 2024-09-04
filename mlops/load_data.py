d4199260cec481ba47ecff0480686d891019f100
hmakumbu <hmboko@aimsammi.org>
2024-09-04 21:27:48 +0000

    creating dataframe

--- mlops/load_data.py
+++ mlops/load_data.py
@@ -2,4 +2,17 @@ from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
+data_iris = load_iris()
+X = data_iris.data 
+y = data_iris.target
+
+def load_train():
+     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+     return X_train, X_test, y_train, y_test
+
+if __name__==  "__main__":
+     
+    df = pd.DataFrame(X)
+    print(df)
