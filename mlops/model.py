de365a7c5e64054c0c504f6837c1b9f599298b5d
hmakumbu <hmboko@aimsammi.org>
2024-09-05 00:39:44 +0000

    adding prediction function and evaluate

--- mlops/model.py
+++ mlops/model.py
@@ -1,4 +1,14 @@
 from sklearn.linear_model import LogisticRegression
+from mlops.load_data import load_train
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score
 
 
-model = LogisticRegression(max_iter=200)  # max_iter is increased to ensure convergence
+X_train, X_test, y_train, y_test = load_train()
+model = LogisticRegression(max_iter=200)
+
+model.fit(X_train,y_train)
+
+def evaluate():
+    prediction = model.predict(X_test)
+    accuracy = accuracy_score(y_true=y_test,y_pred=prediction)
+    return accuracy
