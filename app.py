8ba0c935fc97f4d4f7b24231f93e4e30aee5529d
hmakumbu <hmboko@aimsammi.org>
2024-09-05 02:20:24 +0000

    updating main

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from mlops.load_data import load_train
-from mlops.model import LogisticRegression, evaluate
-from mlops.test_unit import test_unit
+#from mlops.test_unit import test_unit
 from sklearn.datasets import load_iris
+from mlops.model import  ModelRegression
 
 dfiris = load_iris()
 X = dfiris.data
@@ -9,11 +9,18 @@ y = dfiris.target
 
 #PipeLine
 
-#load data
-X_train, X_test, y_train, y_test = load_train(X,y)
+if __name__=="__main__":
 
-#train
+    #load data
+    X_train, X_test, y_train, y_test = load_train(X,y)
 
+    #initialize model
+    model = ModelRegression()
 
+    #train model
+    model.training(X_train=X_train, y_train=y_train)
 
+    #prediction
+    accuracy = model.evaluate(X_test,y_test)
 
+    print(accuracy)
