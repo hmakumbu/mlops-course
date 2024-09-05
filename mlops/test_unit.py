08dbfba9a95f2ddb71db47437b19e65b3026ea19
hmakumbu <hmboko@aimsammi.org>
2024-09-05 02:30:23 +0000

    try

--- mlops/test_unit.py
+++ mlops/test_unit.py
@@ -4,7 +4,16 @@ from sklearn.datasets import load_iris
 data_iris = load_iris()
 X = data_iris.data 
 y = data_iris.target
+X_train, X_test, y_train, y_test = load_train(X,y)
 
+#initialize model
+model = ModelRegression()
+
+#train model
+model.training(X_train=X_train, y_train=y_train)
+
+#prediction
+accuracy = model.evaluate(X_test,y_test
 
 
 def test_unit():
